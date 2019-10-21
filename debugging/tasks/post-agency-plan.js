2970b4aa013cdedd84eba3ae18377e07dfa60787
hkernbach <heiko.kernbach@gmail.com>
2019-10-21 12:46:58 +0200

    post agency plan

---
+++ debugging/tasks/post-agency-plan.js
@@ -0,0 +1,71 @@
+/*jshint globalstrict:false, strict:false, sub: true */
+/*global ARGUMENTS, print, arango, db */
+exports.name = "post-agency-plan";
+exports.group = "Post an agency plan to a new leader agency. Only for debug purpose!";
+exports.args = [
+  {"name": "dump-file", "optional": false, "type": "jsonfile", "description": "agency dump file"}
+];
+exports.args_arangosh = "| --server.endpoint LEADER-AGENT";
+exports.description = "Posts an agency dump to an ArangoDB agency leader.";
+exports.selfTests = ["arango", "db"];
+exports.requires = "3.3.23 - 3.5.99";
+exports.info = `
+This task takes an agency dump file, modificates it to fit to the new server and post it.
+
+ - arangosh --javascript.execute ../debug-scripts/debugging/index.js  post-agency-plan agencyDump.json --server.endpoint tcp://(ip-address):(agency-port)> (agency)
+`;
+
+exports.run = function (extra, args) {
+
+  // imports
+  const helper = require('../helper.js');
+  const _ = require('underscore');
+
+  // variables
+  const file = helper.getValue("dump-file", args);
+  const dump = helper.getAgencyDumpFromObjectOrAgency(file);
+  const agencyPlan = dump.arango.Plan;
+  const health = dump.arango.Supervision.Health;
+
+  let getNamesOfCurrentDatabaseServers = function (role) {
+    let names = [];
+    _.each(health, function (info, serverName) {
+      if (serverName.substring(0, 4) === 'PRMR') {
+        // found dbserver
+        if (info.Status === 'GOOD') {
+          names.push(serverName);
+        }
+      }
+    });
+    return names;
+  };
+
+  let oldDBServernames = getNamesOfCurrentDatabaseServers('PRMR');
+  let x = helper.getAgencyDumpFromObjectOrAgency(undefined);
+
+  let newDBServerNames = [];
+  _.each(x.arango.Plan.DBServers,function (content, name) {
+    newDBServerNames.push(name);
+  });
+
+  let tmp = JSON.stringify(agencyPlan);
+  for (let i = 0; i < oldDBServernames.length; i++) {
+    var re = new RegExp(oldDBServernames[i],"g");
+    tmp = tmp.replace(re, newDBServerNames[i]);
+  }
+  let newAgencyPlan = JSON.parse(tmp);
+  newAgencyPlan.Version = (x.arango.Plan.Version + 1);
+
+  let pre = {};
+  let data = {};
+  data['/arango/Plan'] = newAgencyPlan;
+
+  print("Trying to post agency plan to agency.")
+  let res = helper.httpWrapper('POST', '/_api/agency/write', [[data, pre]]);
+
+  if (res.results[0] === 0) {
+    print("WARNING: pre-condition failed, maybe cleanup already done");
+  } else {
+    print("INFO: " + JSON.stringify(res));
+  }
+};
