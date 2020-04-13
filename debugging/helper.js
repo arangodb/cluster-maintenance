b031618683c1caaf05a1e5f995104ac183f8133f
Frank Celler <frank@arangodb.com>
2020-04-13 13:35:43 +0200

    eslint fixes

--- debugging/helper.js
+++ debugging/helper.js
@@ -1,5 +1,5 @@
-/*jshint globalstrict:false, strict:false, sub: true */
-/*global ARGUMENTS, print, arango, db */
+/* jshint globalstrict:false, strict:false, sub: true */
+/* global print, arango, db */
 const fs = require('fs');
 const _ = require('lodash');
 
@@ -20,14 +20,13 @@ const fatal = (msg) => {
   process.exit(1);
 };
 
-const padRight = function(value, maxLength) {
+const padRight = function (value, maxLength) {
   if (value.length >= maxLength) {
     return value.substr(0, maxLength);
   }
   return value + Array(maxLength - value.length + 1).join(" ");
 };
 // messages - end /////////////////////////////////////////////////////////////
-
 
 // connections ////////////////////////////////////////////////////////////////
 let stringify;
@@ -54,7 +53,8 @@ const httpWrapper = (method, url, payload, headers) => {
   if (!arango[method]) {
     fatal("Unknown HTTP method " + method);
   }
-  if (method === 'GET' || method === 'DELETE') {
+  if (method === 'GET' || method === 'DELETE' ||
+      method === 'GET_RAW' || method === 'DELETE_RAW') {
     return arango[method](url, headers);
   }
 
@@ -66,7 +66,6 @@ const httpWrapper = (method, url, payload, headers) => {
   }
 };
 // connections - end //////////////////////////////////////////////////////////
-
 
 // roles //////////////////////////////////////////////////////////////////////
 const getRole = () => {
@@ -95,7 +94,8 @@ const checkCoordinator = () => {
 
 const checkAgent = () => {
   if (!checkRole("AGENT")) {
-    fatal("Script needs a connection to the leader agent. Currently connected to a " + getRole() + ".");
+    fatal("Script needs a connection to an agent. Currently connected to a " +
+          getRole() + ".");
   }
 };
 
@@ -103,22 +103,25 @@ const checkLeader = () => {
   if (!checkRole("AGENT")) {
     fatal("Script needs a connection to the leader agent. Currently connected to a " + getRole() + ".");
   }
-  const response = httpWrapper('POST', '/_api/agency/read', [["/"]]); // read agency form leader
+  const response = httpWrapper('POST_RAW', '/_api/agency/read', [["/"]]); // read agency form leader
   if (response.code === 307) {
-    fatal("You need to connect to the leader agent");
+    const location = response.headers.location;
+    fatal("You need to connect to the leader agent at '" +
+          location.substr(0, location.length - "_api/agency/read".length) +
+          "'");
   } else if (response.error) {
-    fatal("Got error while checking for leader agency: " + response.errorMessage);
+    fatal("Got error while checking for leader agency: " +
+          response.errorMessage);
   }
   const stores = httpWrapper('GET', '/_api/agency/stores');
-  return [response, stores];
+  return [response.parsedBody, stores];
 };
 // roles - end ////////////////////////////////////////////////////////////////
 
-
 // file reading ///////////////////////////////////////////////////////////////
 const readJsonFile = (file, mustBeRead = false) => {
-  if(file === undefined || !fs.isFile(file)){
-    if(mustBeRead) {
+  if (file === undefined || !fs.isFile(file)) {
+    if (mustBeRead) {
       fatal("Can not read file: '" + file + "'");
     }
     return undefined;
@@ -132,8 +135,7 @@ const readJsonFile = (file, mustBeRead = false) => {
 };
 // file reading - end /////////////////////////////////////////////////////////
 
-
-// agency dumps ///////////////////////////////////////////////////////////////
+// agency dumps and config ////////////////////////////////////////////////////
 const getAgencyDumpFromObject = (content) => {
   // content is a must be the parsed contnet of a json file
   if (content === undefined) {
@@ -149,15 +151,59 @@ const getAgencyDumpFromObject = (content) => {
 const getAgencyDumpFromObjectOrAgency = (obj = undefined) => {
   let agency = getAgencyDumpFromObject(obj);
   let stores;
-  if(agency === undefined) {
+  if (agency === undefined) {
     const response = checkLeader();
     agency = response[0][0];
     stores = response[1];
   }
-  return [agency,stores];
+  return [agency, stores];
 };
-// agency dumps - end /////////////////////////////////////////////////////////
 
+const getAgencyConfiguration = () => {
+  if (!checkRole("AGENT")) {
+    fatal("Script needs a connection to an agent. " +
+          "Currently connected to a " + getRole() + ".");
+  }
+  const url = '/_api/agency/config';
+  const response = httpWrapper('GET_RAW', url);
+  if (response.code === 307) {
+    const location = response.headers.location;
+    fatal("You need to connect to the leader agent at '" +
+          location.substr(0, location.length - url.length) + "'");
+  } else if (response.error) {
+    fatal("Got error while checking for leader agency: " +
+          response.errorMessage);
+  }
+  return response.parsedBody;
+};
+
+const switchToAgencyLeader = () => {
+  if (!checkRole("AGENT")) {
+    fatal("Script needs a connection to an agent. " +
+          "Currently connected to a " + getRole() + ".");
+  }
+  const url = '/_api/agency/read';
+  let response = httpWrapper('POST_RAW', url, [["/"]]);
+  if (response.code === 307) {
+    const location = response.headers.location;
+    const conn = location.substr(0, location.length - url.length);
+    arango.reconnect(conn, "_system");
+
+    if (!checkRole("AGENT")) {
+      fatal("Reconnect did not connect to an agent. " +
+            "Currently connected to a " + getRole() + ".");
+    }
+
+    response = httpWrapper('POST_RAW', url, [["/"]]);
+    if (response.code !== 200) {
+      fatal("Cannot switch to leader: " + response.errorMessage);
+    }
+  } else if (response.error) {
+    fatal("Got error while checking for leader agency: " +
+          response.errorMessage);
+  }
+};
+// agency dumps and config - end //////////////////////////////////////////////
 
 // arguments and usage ////////////////////////////////////////////////////////
 /*
@@ -200,7 +246,7 @@ const printUsage = (task) => {
   let usageArray = task.args;
   let itemToString = (usageObject) => {
     let out = "<" + usageObject.name + ">";
-    if(usageObject.optional) {
+    if (usageObject.optional) {
       out = "[" + out + "]";
     }
     return out;
@@ -215,9 +261,9 @@ const printUsage = (task) => {
                   .reduce((x, y) => { return x + " " + y; }, task.name) + " " + arangosh
        );
   usageArray.map((x) => {
-    if(x.description !== undefined) {
+    if (x.description !== undefined) {
       let delim = "-";
-      if(x.optional === true) {
+      if (x.optional === true) {
         delim = "(optional) -";
       }
       print(`  ${x.name} ${delim} ${x.description}`);
@@ -239,9 +285,9 @@ const checkArgs = (task, args) => {
 
   if (proArgs < reqArgs) {
     printUsage(task);
-    fatal("Not enough arguments - "
-          + proArgs  + " arguments provided while "
-          + reqArgs + " required!");
+    fatal("Not enough arguments - " + proArgs +
+          " arguments provided while " + reqArgs +
+          " required!");
   }
 
   for (let i = 0; i < proArgs; i++) {
@@ -253,7 +299,7 @@ const checkArgs = (task, args) => {
         toSet.value = given;
         break;
       case 'jsonfile':
-        toSet.value = readJsonFile(given, true /*must read*/);
+        toSet.value = readJsonFile(given, true /* must read */);
         break;
       case 'boolean':
       case 'bool':
@@ -278,19 +324,38 @@ const checkArgs = (task, args) => {
   return task.args;
 };
 
-const getValue = (name, args) => {
-  //handle default values here?
-  const rv = args.find( x  => x.name === name );
-  if( rv === undefined) {
-      fatal("Trying to access undefined argument: '" + name + "'");
+const checkGlobalArgs = (args) => {
+  const options = {
+    findLeader: false
   };
-  return rv.value;
+
+  let l = args.length;
+
+  for (let i = 1; i < l; i++) {
+    let name = args[i];
+    let found = 0;
+
+    if (name === '--find-leader') {
+      options.findLeader = true;
+      found = 1;
+    }
+
+    if (found > 0) {
+      args.splice(i, found);
+      l -= found;
+    }
+  }
+
+  return options;
 };
 
-const getType = (name, args) => {
-  const rv = args.find( x  => x.name === name );
-  if( rv === undefined) { return rv; };
-  return rv.type;
+const getValue = (name, args) => {
+  // handle default values here?
+  const rv = args.find(x => x.name === name);
+  if (rv === undefined) {
+      fatal("Trying to access undefined argument: '" + name + "'");
+  }
+  return rv.value;
 };
 // arguments and usage - end //////////////////////////////////////////////////
 
@@ -363,7 +428,7 @@ let extractDatabases = function (info, dump) {
             collection: collection.name
           };
 
-          if (0 < shard.length) {
+          if (shard.length > 0) {
             coll.leaders.push(shard[0]);
             setGlobalShard(info,
               _.extend({
@@ -458,13 +523,18 @@ exports.httpWrapper = httpWrapper;
 exports.checkCoordinator = checkCoordinator;
 exports.getRole = getRole;
 exports.checkLeader = checkLeader;
+exports.checkAgent = checkAgent;
 
 // file reading
 exports.readJsonFile = readJsonFile;
-// agency dump
+
+// agency dump and config
 exports.getAgencyDumpFromObjectOrAgency = getAgencyDumpFromObjectOrAgency;
+exports.getAgencyConfiguration = getAgencyConfiguration;
+exports.switchToAgencyLeader = switchToAgencyLeader;
 
 // arguments and usage
 exports.printUsage = printUsage;
 exports.checkArgs = checkArgs;
+exports.checkGlobalArgs = checkGlobalArgs;
 exports.getValue = getValue;
